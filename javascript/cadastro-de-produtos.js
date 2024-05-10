1e88db2346587c02ac2a752897c326736529b50e
Frederico Enzo <portifolio.frederico@gmail.com>
2024-05-10 19:09:33 -0300

    mudanças no estilo e novas telas

--- javascript/cadastro-de-produtos.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("home").addEventListener("click", function() {
-    window.location.href = "../html/dashboard.html";
-});
--- javascript/dashboard.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("cadastro-de-produtos").addEventListener("click", function() {
-    window.location.href = "../html/cadastro-de-produtos.html";
-});
